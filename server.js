e5e68c917dbc6d82568516b852adddb098289017
李嘉豪 <199506@nd.com>
2018-05-07 19:17:12 +0800

    '增加入口文件'

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const express = require('express')
+const webpack = require('webpack')
+const rewriteJson = require('./mock/routes.json')
+const config = require('./webpack.route')
+
+let app = express()
+
+const compiler = webpack(config)
+
+// set static dir
+app.use(express.static(path.join(__dirname, '/')))
+
+app.use(require('webpack-dev-middleware')(compiler), {
+  npInfo: true,
+  publicPath: config.output.publicPath
+})
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+if (process.argv.join(' ').indexOf(' --mock') > 0) {
+  const jsonServer = require('json-server')
+  let router = jsonServer.router('./mock/db.json')
+  let middlewares = jsonServer.defaults()
+  let rewriter = jsonServer.rewriter(rewriteJson)
+  app.use(middlewares)
+  app.use(rewriter)
+  app.use(router)
+}
+
+app.listen(config.port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  var uri = 'http://localhost:' + config.port
+  console.log('Listening at ' + uri + '\n')
+  open(uri)
+})
